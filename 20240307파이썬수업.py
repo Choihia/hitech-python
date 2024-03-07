51aea12d8fc22fa7125f3df2a5389c0b89d8ad67
Choihia <162548192+Choihia@users.noreply.github.com>
2024-03-07 16:14:51 +0900

    Add files via upload

---
+++ 20240307파이썬수업.py
@@ -0,0 +1,45 @@
+#print("Hello, world")
+#a = 7
+#b = 3
+#c= a + b
+#d = a - b
+#e = a * b
+#f = a / b
+#g = a // b
+#h = a % b
+#print(c)
+#20240307
+#주석 다는 법
+#print("동해물과 백두산이 마르고 닳도록 하느님이 보우하사 우리나라 만세")
+#korea = "동해물과 백두산이 마르고 닳도록 하느님이 보우하사 우리나라 만세"
+#print(korea)
+#print(c)
+#print(d)
+#print(e)
+#print(f)
+#print(g)
+#print(h)
+#a1 = 3 
+#if a1 == 3:
+#    print("a1는 3이다")
+#a *= 10
+#print(a)
+#a -= 20
+#print(a)
+#a += 10
+#print(a)
+#a //= 7
+#print(a)
+
+x = True
+y = False
+
+#if x and not x:
+#    print("No")
+#else:
+#    print("Yes")
+
+aa = [1,2,3,4,5]
+bb = 1 in aa
+#print(bb)
+
